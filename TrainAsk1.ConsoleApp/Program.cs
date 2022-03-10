b621e14528ee0870b393f5f0dcab6d0187e7d0a9
Vasilis Pierros <pierrosv@hotmail.com>
2022-03-10 23:17:14 +0200

    Add project files.

---
+++ TrainAsk1.ConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TrainAsk1.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int seqCounter = 0;
+        int entityCounter = 0;
+
+        // Read the file and display it line by line.  
+        using StreamWriter file = new(@"C:\covid\HBV_single.fasta");
+
+        foreach (string line in System.IO.File.ReadLines(@"C:\covid\HBV.fasta"))
+        {
+            if (line.StartsWith(">"))
+            {
+                if (entityCounter > 0)
+                {
+                    System.Console.WriteLine("");
+                    file.WriteLine("");
+                }
+                entityCounter++;
+                {
+                    System.Console.WriteLine(line);
+                    file.WriteLine(line);
+                }
+            }
+            else
+            {
+                {
+                    System.Console.Write(line);
+                    file.Write(line);
+                }
+                seqCounter++;
+            }
+        }
+        file.Close();
+
+        System.Console.WriteLine("There were {0} entities.", entityCounter);
+        System.Console.WriteLine("There were {0} sequences.", seqCounter);
+        System.Console.ReadLine();
+    }
+}
